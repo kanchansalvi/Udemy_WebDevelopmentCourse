faa31e79895a28dfaf11e61add0b09293d62ff07
kanchansalvi <33441738+kanchansalvi@users.noreply.github.com>
2019-10-30 17:45:24 -0700

    Project update

--- Projects/3.Fruits Slice game(HTML, CSS & jQuery/jquery.js
+++ Projects/3.Fruits Slice game(HTML, CSS & jQuery/jquery.js
@@ -25,14 +25,16 @@
 
 var playing = false;
 var score = 0;
-$(function)(){
+var	attemptsLeft = 3;
+var fruits = ['apple', 'orange', 'banana', 'cherry', 'grape', 'mango', 'pear'];
+$(function(){
 	
-		$("#startReset").click(function(){
+	$("#startReset").click(function(){
 			//	Is game ongoing?
 			 if(playing == true){
 //				Yes -> Continue the game
   					location.reload();
-//				} 
+				} 
 //			No -> Start the Game
 //								Change button text to Reset
 //								Set Score to 0
@@ -41,16 +43,65 @@ $(function)(){
 						score = 0;
 						$("#scorevalue").html("09");
 //				Show trials left
-				$("#attemptsLeft").show();
-				var	attemptsLeft = 3;
-				addHearts();
+				  $("#attemptsLeft").show();
+						addHearts();
+				  startAction();
 				}
 		});
-}
+});
+
+$("#fruit").mouseover(function(){
+				score++;
+				$("#scorevalue").html(score);
+//	
+//	 //stop fruit     
+	clearInterval(action); 
+//	
+	   $("#fruit").hide() ;
+				setTimeout(startAction(), 500);
+//	
+});
+
 
 function addHearts(){
-	for(i=0; i<trialsLeft; i++){
-		$("#trialsLeft").append(" X ");
+	for(i=0; i<attemptsLeft; i++){
+		$("#attemptsLeft").append('<img src="images/heart.png">  ');
 	}
 	
+}
+
+function startAction(){
+//	$("#fruit").show();
+	generateFruit();
+	moveFruit();
+
+}
+
+function generateFruit(){
+	// Get a random fruit
+	$("#fruit").attr('src', 'images/' + fruits[Math.round(7*Math.random())] + '.png');
+	
+		// Show the fruit at random position
+		$("#fruit").css({'left': Math.round(900*Math.random()), 'top': -60});
+	$("#fruit").show();
+	
+	// Get random step for fruit to move down
+	step = 1 + Math.round(5*Math.random());
+}
+
+function moveFruit(){
+	// Move fruit down every 10 ms
+	action = setInterval(function(){
+	
+	// Move fruit by 1 step
+	$("#fruit").css({'top': $("#fruit").position().top+step});
+		
+		if($("#fruit").position().top > $("#fruitContainer").height()){
+			
+					if(attemptsLeft > 1){
+						$("#fruit").show();
+						generateFruit();
+					}
+		}
+},10);
 }
