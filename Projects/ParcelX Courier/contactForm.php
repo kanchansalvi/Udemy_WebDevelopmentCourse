2686aa9a92adb13232eda10020333a9a5ffabbe3
kanchansalvi <33441738+kanchansalvi@users.noreply.github.com>
2019-12-21 23:36:24 -0800

    update

---
+++ Projects/ParcelX Courier/contactForm.php
@@ -0,0 +1,14 @@
+<?php
+// If user has submitted the form
+if($_POST["submit"])
+	// check for errors
+   // If errors
+     // Print error message
+   // If no errors
+     // send the email
+        // I sent successfully
+           // Print success
+        // fail
+           // print warning message
+
+?>
